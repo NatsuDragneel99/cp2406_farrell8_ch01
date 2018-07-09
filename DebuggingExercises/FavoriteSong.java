00590b13d34ef093f36d8b69996c42ea5bc13a97
NatsuDragneel99 <37394593+NatsuDragneel99@users.noreply.github.com>
2018-07-09 14:53:10 +0800

    Final Pratical 1

---
+++ DebuggingExercises/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Sing");
+        System.out.println("Somebody");
+        System.out.println("Hello,");
+        System.out.println("Where");
+        System.out.println("say something");
+    }
+}
---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,8 @@
+public class MovieQuote
+{
+    /* This program displays a greeting */
+    public static void main(String[] args)
+    {
+        System.out.println("Now or never");
+    }
+}
